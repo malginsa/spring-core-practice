46cbd5db52a9d8a4e3847dc4cdcc153802259ec1
Sergei Malgin <Sergei_Malgin@epam.com>
2017-02-12 23:12:29 +0300

    first commit

---
+++ src/main/java/com/epam/spring/core/App.java
@@ -0,0 +1,19 @@
+package com.epam.spring.core;
+
+public class App {
+
+    private Client client;
+    private EventLogger eventLogger;
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.client = new Client("1", "John Smith");
+        app.eventLogger = new ConsoleEventLogger();
+        app.logEvent("Some event for user 1");
+    }
+
+    public void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        eventLogger.logEvent(message);
+    }
+}
